7240c2c991c0aa0b2565ed09ad4665b7d52659fc
abene42 <abene42@gmail.com>
2021-12-28 21:51:13 +0300

    fix: added jquery

--- public/js/f_co.js
+++ public/js/f_co.js
@@ -18,14 +18,6 @@ function download_file() {
     let gsReference = storage.refFromURL('gs://f-co-507ef.appspot.com/DB Request.pdf');
     gsReference.getDownloadURL()
         .then((url) => {
-            // var xhr = new XMLHttpRequest();
-            // xhr.responseType = 'blob';
-            // xhr.onload = (event) => {
-            //     var blob = xhr.response;
-            // };
-            // xhr.open('GET', url);
-            // xhr.send();
-
             $.ajax({
                 method: 'GET',
                 url: '/download',
@@ -37,34 +29,12 @@ function download_file() {
                 },
                 success: function (response) {
                     console.log(response)
-                    // let blob = new Blob([response], { type: "application/pdf" });
-                    //
-                    // let a = document.createElement('a');
-                    // a.href = window.URL.createObjectURL(blob);
-                    // a.download = "document.pdf";;
-                    // document.body.appendChild(a);
-                    // a.click();
-                    // document.body.removeChild(a);
-                    // window.URL.revokeObjectURL(a.href);
                 },
                 error: function (xhr, ajaxOptions, thrownError) {
                     console.log(thrownError)
                 },
                 timeout: 8000
             })
-            // fetch(url)
-            //     .then(resp => resp.blob())
-            //     .then(blob => {
-            //         const url = window.URL.createObjectURL(blob);
-            //         const a = document.createElement('a');
-            //         a.style.display = 'none';
-            //         a.href = url;
-            //         // the filename you want
-            //         a.download = 'blah.PDF';
-            //         document.body.appendChild(a);
-            //         a.click();
-            //         window.URL.revokeObjectURL(url);
-            //     })
-            //     .catch(() => alert('oh no!'));
+
         })
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,8 +46,50 @@
         <!-- /page content -->
     </div>
 </div>
-<script src="{{asset('https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap.min.js')}}"></script>
-<script src="{{asset('js/f_co.js')}}"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    $(document).ready(function () {
+        $('.dt_table').DataTable();
+        const firebaseConfig = {
+            apiKey: "AIzaSyAnRnx7c6CD7-rA_erIJXScSLoGYDXHhZk",
+            authDomain: "f-co-507ef.firebaseapp.com",
+            projectId: "f-co-507ef",
+            storageBucket: "f-co-507ef.appspot.com",
+            messagingSenderId: "219491375371",
+            appId: "1:219491375371:web:b6c4c0257daa7beff9aa0e",
+            measurementId: "G-KNB6VV9DTQ"
+        };
+        firebase.initializeApp(firebaseConfig);
+    })
+
+    function download_file() {
+        console.log('From download file function')
+        let storage = firebase.storage();
+        let gsReference = storage.refFromURL('gs://f-co-507ef.appspot.com/DB Request.pdf');
+        gsReference.getDownloadURL()
+            .then((url) => {
+                $.ajax({
+                    method: 'GET',
+                    url: '/download',
+                    data: {
+                        url: url
+                    },
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function (response) {
+                        console.log(response)
+                    },
+                    error: function (xhr, ajaxOptions, thrownError) {
+                        console.log(thrownError)
+                    },
+                    timeout: 8000
+                })
+
+            })
+    }
+</script>
 </body>
 </html>
 
